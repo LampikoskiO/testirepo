fdb49faa5c67e2812a026cb83a9fd14cf43b887e
onni testi <testi@testi1.com>
2021-12-27 17:28:59 +0200

    eka

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Harjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sanap = 0;
+            string[] tervehdys = new string[] {"mo", "moi", "hei", "haloo"};
+            for (int i = 0; i < tervehdys.Length; i++) {
+                sanap = tervehdys[i].Length;
+              //  Console.WriteLine(sanap);
+                if (sanap >= 3){
+                Console.WriteLine(tervehdys[i]);
+                }
+            }
+        }
+    }
+}
